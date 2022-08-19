7cfb7990ea8cb41757fc62b5e2eeb3fbb4369542
saraHH1417 <sara.afzar@gmail.com>
2022-08-19 22:23:51 +0430

    hello world and number data types

---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int cats;
+        int dogs = 9;
+        float height = 1.6f;
+        double electronSize = 4.787885;
+        cats = 7;
+        int total = cats + dogs;
+
+
+        System.out.println("Number of cats: " +  cats);
+        System.out.println("Number of dogs: "+ dogs);
+        System.out.println("Total number of animals: " + total);
+        System.out.println("Int divide: " + 10/ 3);
+        System.out.println("Float divide:" + 10.0 / 3.0);
+        System.out.println("Height * 7: "+ height * 7);
+        System.out.println("electron size: " + electronSize);
+    }
+}
