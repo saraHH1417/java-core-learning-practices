299a875a7ae4fd5979f5ccea78806488e2ff274e
saraHH1417 <sara.afzar@gmail.com>
2022-08-29 01:06:32 +0430

    egg timer practice

--- src/FurtherOOP/MultipleInheritance/App.java
+++ src/FurtherOOP/MultipleInheritance/App.java
@@ -1,14 +1,24 @@
 package FurtherOOP.MultipleInheritance;
 
+import javax.swing.*;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 class Task extends TimerTask {
     public void run() {
-        System.out.println("Hello!");
+        JOptionPane.showMessageDialog(null, "Egg cooked");
     }
 }
 
+class TaskInterface implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Timer running");
+    }
+}
 public class App {
     public static void main(String[] args) {
         System.out.println("In java we don't have multiple inheritance," +
@@ -19,6 +29,12 @@ public class App {
 
         System.out.println("\n\n example abstract class");
         Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new Task(), 0L, 1000L);
+        timer.scheduleAtFixedRate(new Task(),  20000L, 20000L);
+
+        System.out.println("\n \n Here we have interface example");
+        ScheduledExecutorService executor =  Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(new TaskInterface(), 0, 10L, TimeUnit.SECONDS);
+
+
     }
 }
